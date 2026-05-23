fe1dbdb204067b2bc16dfc0696bf3c6dc3fccf4c
Sai Krishna Resu <143726106+saikrishnaresuk@users.noreply.github.com>
2026-05-23 14:25:53 +0530

    Update EKS End to end Project.sh

--- EKS End to end Project.sh
+++ EKS End to end Project.sh
@@ -5,14 +5,20 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
+# installing Eksctl
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz"
 tar -xzf eksctl_*.tar.gz
 sudo mv eksctl /usr/local/bin
 eksctl version
+# installing Aws cli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+# configuring Aws cli
 aws configure
+# Creating Cluster
 eksctl create cluster --name sai-cluster --region us-east-1 --fargate
+# updating kube config file
 aws eks update-kubeconfig --name sai-cluster --region us-east-1
+# Creatting fargate profile
 eksctl create fargateprofile     --cluster sai-cluster     --region us-east-1     --name alb-sample-app     --namespace game-2048
